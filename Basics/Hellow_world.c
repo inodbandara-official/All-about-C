fa410548c807def48514c297ec724bacf2adb61a
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-20 14:21:13 +0530

    Create Hellow_world.c
    Included comments for each line.

---
+++ Basics/Hellow_world.c
@@ -0,0 +1,16 @@
+// we can write comments using two foward slashes (" // ")
+
+// Simple C program to display "Hello World"
+// Header file for input output functions
+#include <stdio.h>
+
+// this is the main function
+// where the execution of program begins
+int main()
+{
+
+	// you can prints hellow world by using printf command
+	printf("Hellow world");
+  // we can ends the execution of this function using this command line
+	return 0;
+}
