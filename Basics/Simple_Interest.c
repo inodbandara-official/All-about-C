e8cc20fd17bc238c82f8e7079821a3c7aa3bcf2d
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-22 22:00:02 +0530

    Create Simple_Interest.c
    Must add the comments. Still need some updates.

---
+++ Basics/Simple_Interest.c
@@ -0,0 +1,17 @@
+// C program to implement the above approach
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	// We can change values here for different inputs
+	float P = 1, R = 1, T = 1;
+
+	// Calculate simple interest
+	float SI = (P * T * R) / 100;
+
+	// Print Simple Interest
+	printf("Simple Interest = %f\n", SI);
+
+	return 0;
+}
