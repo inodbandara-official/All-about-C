cbd79382965ccb044f8a691247766b9f3a1ca505
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-08-23 12:02:03 +0530

    Update halfNum_pyramid.c
    Must update the comments.

--- Patterns/halfNum_pyramid.c
+++ Patterns/halfNum_pyramid.c
@@ -1,14 +1,14 @@
-// C program to print right half pyramid pattern of star
+// C program to print right half pyramid pattern of number/s and star/s
 #include <stdio.h>
 
 int main()
 {
-	int rows;
+	int row;
 	printf("Number of rows: ");
-	scanf("%d", &rows);
+	scanf("%d", &row);
 
 	// first loop for printing rows
-	for (int i = 1; i <= rows; i++) {
+	for (int i = 1; i <= row; i++) {
 
 		// second loop for printing similar number in each rows
 		for (int j = 1; j <= i; j++) {
@@ -16,5 +16,35 @@ int main()
 		}
 		printf("\n");
 	}
+
+	int m = 6;
+
+	// kth row has k elements
+	for (int k = 1; k <= m; k++) {
+		for (int j = 1; l <= k; l++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	int rows = 8, a, b, space;
+
+	for (a = rows; a >= 1; --a)
+	{
+	// Loop to print the blank space
+	for (space = 0;
+			space < rows - a; ++space)
+		printf(" ");
+	
+	// Loop to print the half of the star triangle
+	for (b = a; b <= 2 * a - 1; ++b)
+		printf("* ");
+	
+	// Loop to print the rest of the star triangle
+	for (b = 0; b < a - 1; ++b)
+		printf("* ");
+	printf("\n");
+}
+
 	return 0;
 }
