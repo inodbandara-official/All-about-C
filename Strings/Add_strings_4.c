c2a8382a5b48c3861cb259eb190cded7a05a1359
Inod Bandara <87967504+inodbandara-official@users.noreply.github.com>
2023-09-01 10:59:13 +0530

    Create Add_strings_4.c

---
+++ Strings/Add_strings_4.c
@@ -0,0 +1,15 @@
+// C program to concatenate two strings using strcat function
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+	char s[] = "Geeks";
+	char s1[] = "forGeeks";
+
+	// concatenating the string
+	strcat(s, s1);
+	printf("Final string is: %s ", s);
+	return 0;
+}
